000f1598756ec571af925429adbf59651bcd19b7
Gustavo Gobetti <gustavo.gobetti98@gmail.com>
2019-04-08 16:13:28 -0300

    tc rtc e rtt implementados

--- SAME70_FONTS/SAME70_FONTS/src/main.c
+++ SAME70_FONTS/SAME70_FONTS/src/main.c
@@ -11,19 +11,32 @@
 #include "calibri_36.h"
 #include "arial_72.h"
 
+//Definindo horario
+#define YEAR        2019
+#define MOUNTH      4
+#define DAY         8
+#define WEEK        12
+#define HOUR        15
+#define MINUTE      45
+#define SECOND      0
+
 //Definindo botao Oled
 #define EBUT1_PIO PIOD //start EXT 9 PD28
 #define EBUT1_PIO_ID 16
 #define EBUT1_PIO_IDX 28
 #define EBUT1_PIO_IDX_MASK (1u << EBUT1_PIO_IDX)
 
-// Flags para velocidade inst, tempo total, distancia total
+// Flags para velocidade inst, tempo total, distancia total variaveis globais
 
+volatile Bool f_rtt_alarme = false;
+volatile Bool f_rtc_alarme = false;
 volatile Bool but_flag;
 volatile Bool but_start;
-int velocidade = 0;
-int distanciaTotal = 0;
-int tempoTrechoTotal = 0;
+char velocidade[32];
+char distanciaTotal[32];
+char tempoTrechoTotal[32];	
+
+void pin_toggle(Pio *pio, uint32_t mask);
 
 
 void but_flag(void){
@@ -58,13 +71,12 @@ void io_init(void){
 	WDT -> WDT_MR_WDDIS;
 }
 
-	void TC_init(Tc * TC, int ID_TC, int TC_CHANNEL){
+	void TC_init(Tc * TC, int ID_TC, int TC_CHANNEL, int freq){
 	uint32_t ul_div;
 	uint32_t ul_tcclks;
 	uint32_t ul_sysclk = sysclk_get_cpu_hz();
 
 	uint32_t channel = 1;
-
 
 	/* Configura o PMC */
 	/* O TimerCounter ? meio confuso
@@ -75,7 +87,7 @@ void io_init(void){
 	*/
 	pmc_enable_periph_clk(ID_TC);
 
-	/** Configura o TC para operar em  4Mhz e interrup?c?o no RC compare */
+	/** Configura o TC para operar em  4hz e interrup?c?o no RC compare */
 	tc_find_mck_divisor(freq, ul_sysclk, &ul_div, &ul_tcclks, ul_sysclk);
 	tc_init(TC, TC_CHANNEL, ul_tcclks | TC_CMR_CPCTRG);
 	tc_write_rc(TC, TC_CHANNEL, (ul_sysclk / ul_div) / freq);
@@ -96,7 +108,82 @@ void pin_toggle(Pio *pio, uint32_t mask){
 }
 volatile uint8_t flag_start = 1;
 
+// Interrupcoes
+void TC1_Handler(void){
+	volatile uint32_t ul_dummy;
 
+	/****************************************************************
+	* Devemos indicar ao TC que a interrup??o foi satisfeita.
+	******************************************************************/
+	ul_dummy = tc_get_status(TC0, 1);
+
+	/* Avoid compiler warning */
+	UNUSED(ul_dummy);
+
+	/** converte as frequencias em omega(velocidade angular) */
+	
+}
+void TC_init(Tc * TC, int ID_TC, int TC_CHANNEL, int freq);
+//Interrupcao 2 
+
+void RTT_Handler(void)
+{
+	uint32_t ul_status;
+
+	/* Get RTT status */
+	ul_status = rtt_get_status(RTT);
+
+	/* IRQ due to Time has changed */
+	if ((ul_status & RTT_SR_RTTINC) == RTT_SR_RTTINC) {  }
+
+	/* IRQ due to Alarm */
+	if ((ul_status & RTT_SR_ALMS) == RTT_SR_ALMS) {
+		//Transformar frequencia em velocidade para calcular tudo
+		f_rtt_alarme = true;                  // flag RTT alarme
+	}
+}
+//3 interrupcao
+void RTC_Handler(void)
+{
+	uint32_t ul_status = rtc_get_status(RTC);
+
+	uint32_t hour, minute, second;
+	/*
+	*  Verifica por qual motivo entrou
+	*  na interrupcao, se foi por segundo
+	*  ou Alarm
+	*/
+	if ((ul_status & RTC_SR_SEC) == RTC_SR_SEC) {
+		rtc_clear_status(RTC, RTC_SCCR_SECCLR);
+	}
+	
+	/* Time or date alarm */
+	if ((ul_status & RTC_SR_ALARM) == RTC_SR_ALARM) {
+		rtc_clear_status(RTC, RTC_SCCR_ALRCLR);
+
+		if(flag_led0 == 1){
+			flag_led0 = 0;
+			rtc_get_time(RTC, &hour, &minute, &second);
+			rtc_set_time_alarm(RTC, 1, hour, 1, minute, 1, second+2);
+			tc_start(TC0, 1);
+
+			
+		}
+		else if(flag_led0 == 0){
+			flag_led0 = 1;
+			rtc_get_time(RTC, &hour, &minute, &second);
+			rtc_set_time_alarm(RTC, 1, hour, 1, minute, 1, second+2);
+			tc_stop(TC0, 1);
+		}			
+	}
+	
+	rtc_clear_status(RTC, RTC_SCCR_ACKCLR);
+	rtc_clear_status(RTC, RTC_SCCR_TIMCLR);
+	rtc_clear_status(RTC, RTC_SCCR_CALCLR);
+	rtc_clear_status(RTC, RTC_SCCR_TDERRCLR);
+	
+}
+void TC_init(Tc * TC, int ID_TC, int TC_CHANNEL, int freq);
 
 
 
@@ -137,10 +224,58 @@ int main(void) {
 	sysclk_init();	
 	configure_lcd();
 	
+	
+	
+	
+	
 	font_draw_text(&sourcecodepro_28, "OIMUNDO", 50, 50, 1);
 	font_draw_text(&calibri_36, "Oi Mundo! #$!@", 50, 100, 1);
 	font_draw_text(&arial_72, "102456", 50, 200, 2);
 	while(1) {
+		if (f_rtt_alarme){
+      
+      /*
+       * O clock base do RTT ? 32678Hz
+       * Para gerar outra base de tempo ? necess?rio
+       * usar o PLL pre scale, que divide o clock base.
+       *
+       * Nesse exemplo, estamos operando com um clock base
+       * de pllPreScale = 32768/32768/2 = 2Hz
+       *
+       * Quanto maior a frequ?ncia maior a resolu??o, por?m
+       * menor o tempo m?ximo que conseguimos contar.
+       *
+       * Podemos configurar uma IRQ para acontecer quando 
+       * o contador do RTT atingir um determinado valor
+       * aqui usamos o irqRTTvalue para isso.
+       * 
+       * Nesse exemplo o irqRTTvalue = 8, causando uma
+       * interrup??o a cada 2 segundos (lembre que usamos o 
+       * pllPreScale, cada incremento do RTT leva 500ms (2Hz).
+       */
+      uint16_t pllPreScale = (int) (((float) 32768) / 2.0);
+      uint32_t irqRTTvalue  = 4;
+      
+      // reinicia RTT para gerar um novo IRQ
+      RTT_init(pllPreScale, irqRTTvalue);       
+	  // Cria um alarme 
+	  rtc_set_date_alarm(RTC, 1, MOUNTH, 1, DAY);
+	  rtc_set_time_alarm(RTC, 1, HOUR, 1, MINUTE, 1, SECOND+2);  
+      
+     /*
+      * caso queira ler o valor atual do RTT, basta usar a funcao
+      *   rtt_read_timer_value()
+      */
+      
+      /*
+       * CLEAR FLAG
+       */
+      f_rtt_alarme = false;
+	  
+	  if(f_rtc_alarme);
+	  
+      
 		
+		}
+}	
 	}
-}
